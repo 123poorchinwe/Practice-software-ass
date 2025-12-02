609a66907c9615a04e2c52cdee23c510ef1971a2
Qinwei Zhu <3098426529@qq.com>
2025-12-02 23:26:16 +0100

    udate 3

--- Ass_3_LMS/src/Book.java
+++ Ass_3_LMS/src/Book.java
@@ -1,4 +1,21 @@
 
-public class Book {
-
+public class Book extends LibraryItem {
+	String author;
+    int numPages;
+    public Book(String title, String publisher, int year, String author, int numPages) {
+        super(title, publisher, year);
+        this.author = author;
+        this.numPages = numPages;
+    }
+    @Override
+    public void displayDetails() {
+        System.out.println("Book Details:");
+        System.out.println("Title: " + title);
+        System.out.println("Publisher: " + publisher);
+        System.out.println("Year: " + year);
+        System.out.println("Author: " + author);
+        System.out.println("Number of Pages: " + numPages);
+        System.out.println(); 
+    }
 }
+
--- Ass_3_LMS/src/LibraryItem.java
+++ Ass_3_LMS/src/LibraryItem.java
@@ -1,4 +1,13 @@
 
-public class LibraryItem {
+public abstract class LibraryItem {
+	String title;
+    String publisher;
+    int year;
+    public LibraryItem(String title, String publisher, int year) {
+        this.title = title;
+        this.publisher = publisher;
+        this.year = year;
+    }
 
+    public abstract void displayDetails();
 }
